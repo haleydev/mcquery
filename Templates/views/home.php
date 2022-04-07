64ef64c5ef3970aaa95ec0a1b261f1fb5dec794b
Warley Rodrigues <warleyhacker@hotmail.com>
2022-04-07 17:15:52 -0300

    update link

--- Templates/views/home.php
+++ Templates/views/home.php
@@ -2,21 +2,9 @@
    <img class="logo" src="<?=URL?>/images/logo.png">
   
    <div class="info">
-      <p>Bem vindo ao mcquery, leia a <a href="#">documentação</a> e crie algo grande!</p>
+      <p>Bem vindo ao mcquery, leia a <a href="https://github.com/haleydev/mcquery">documentação</a> e crie algo grande!</p>
    </div>
 
    <div class="div-versão">
       <p class="versao">mcquery v1.0 - PHP v8.1</p>
    </div>
-
-   <?php
-      // use App\Mailer;
-      // $teste = new Mailer;
-      // $conteudo = "<h1>Bem vindo ao email haley</h1><br>parabens";
-
-      // $teste->send("haleytrader@hotmail.com","haley trder","respostas 2",$conteudo,null,"Public/images/logo.png");
-      // $teste->send("warleyhacker@hotmail.com","warley hacker","respostas 2",$conteudo,null,"Public/images/logo.png");
-      // $teste->send("iprojectk@live.com","warley hacker","respostas 2",$conteudo,null,"Public/images/logo.png");
-
-      // echo $teste->result     
-   ?>
