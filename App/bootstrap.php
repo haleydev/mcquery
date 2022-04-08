cc65d79e09566e2de8aac88c06fae9fd8030b737
Warley Rodrigues <warleyhacker@hotmail.com>
2022-04-08 04:39:14 -0300

    update

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
+if(!file_exists("./.env") or !file_exists("./vendor")){
+    die("Aplicação não iniciada! use o comando 'php mcquery env' para criar o arquivo de configuração e instalar dependências.");
+}
+
 ob_start();
 
 if(!isset($_SESSION)){
     session_start();    
-}
-
-if(!file_exists("./.env") or !file_exists("./vendor")){
-    die("Aplicação não iniciada! use o comando 'php mcquery env' para criar o arquivo de configuração e instalar dependências.");
 }
 
 require 'App/Mcquery/env.php';
--- router.php
+++ router.php
@@ -5,10 +5,14 @@ use Controllers\{ErrorController, HomeController, SitemapController};
 // -------------------------------------------------------------------------
 
 
-$router->url("", function(){(new HomeController)->render();})->name('home');
+$router->url("", function(){
+    (new HomeController)->render();
+})->name('home');
 
 // sitemap dinamico 'pode ser removido caso não seja necessário'
-$router->url("sitemap.xml", function(){(new SitemapController)->sitemap();})->name('sitemap');
+$router->url("sitemap.xml", function(){
+    (new SitemapController)->sitemap();
+})->name('sitemap');
 
 
 
